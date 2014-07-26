53f254058990f6db12c84670e89ad3dbd67ad011
nyghtowl <warrick.melanie@gmail.com>
2014-07-26 12:50:04 -0700

    Added pkgs for import.

--- movie_rec.py
+++ movie_rec.py
@@ -3,11 +3,15 @@ Example Movie Recommender
 
 '''
 
+import itertools
+import json
+import pandas as pd
+
 class Recommender:
 
     def __init__(self, file):
         self.product_data = json.loads(file)
-        self.users = set() #self._find_unique_users()
+        self.users = self._find_unique_users()
 
     def _find_unique_users(self):
         user_list = [[review['customer'] for review in product['reviews']] for product_id, product in self.product_data.iteritems()]
