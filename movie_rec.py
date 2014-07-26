015fcb6792a8b0954db8a6010623f6d6430fc4c8
nyghtowl <warrick.melanie@gmail.com>
2014-07-26 12:32:27 -0700

    Rework as set but added method for unique users.

--- movie_rec.py
+++ movie_rec.py
@@ -7,4 +7,8 @@ class Recommender:
 
     def __init__(self, products_file):
         self.product_data = json.loads(products_file)
-        self.users = self._find_unique_users()
+        self.users = set() #self._find_unique_users()
+
+    def _find_unique_users(self):
+        user_list = [[review['customer'] for review in product['reviews']] for product_id, product in self.product_data.iteritems()]
+        return set(itertools.chain(*user_list))
