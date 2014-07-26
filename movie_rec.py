05fa039fc504faeb8641f14716495e0dda905238
nyghtowl <warrick.melanie@gmail.com>
2014-07-26 12:52:22 -0700

    Added create user table original that uses just dict.

--- movie_rec.py
+++ movie_rec.py
@@ -16,3 +16,10 @@ class Recommender:
     def _find_unique_users(self):
         user_list = [[review['customer'] for review in product['reviews']] for product_id, product in self.product_data.iteritems()]
         return set(itertools.chain(*user_list))
+
+    def create_user_table(self):
+        user_table = {}
+        for i, user in enumerate(self.users):
+            user_table[user] = i
+        self.lookup_user = user_table
+
